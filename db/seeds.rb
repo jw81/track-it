bf3f0aa4c3017c8ba81755b584d0896ad928dc0f
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2019-12-20 13:01:44 -0600

    Add seed data
    - Also modify the location selection element so it includes the full location data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+opponent_names = [
+  'Minutemen (Lexington)',
+  'Panthers (Oak Grove)',
+  'Spartans (Richmond)',
+  'Owls (Windsor)',
+  'Bulldogs (Brookfield)',
+  'Cardinals (Lawson)',
+  'Mules (Lathrop)',
+  'Owls (Marshall)'
+]
+
+puts "Creating #{opponent_names.count} Opponent records..."
+opponent_names.each do |opponent_name|
+  Opponent.create(name: opponent_name)
+end
+puts 'Finished creating Opponent records.'
+
+
+
+locations = [
+  { city: 'Higginsville', state: 'MO', description: 'High School Gym' },
+  { city: 'Higginsville', state: 'MO', description: 'Middle School Gym' },
+  { city: 'Oak Grove', state: 'MO', description: 'High School Gym' },
+  { city: 'Marshall', state: 'MO', description: 'High School Gym' },
+  { city: 'Lexington', state: 'MO', description: 'High School Gym' }
+]
+
+puts "Creating #{locations.count} Location records..."
+locations.each do |location|
+  Location.create(location)
+end
+puts 'Finished creating Location records.'
