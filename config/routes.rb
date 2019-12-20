436e37a6a3894c6fb2966034158012a7634ffa5f
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2019-12-20 13:21:00 -0600

    Add a root route to make Heroku happy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'games#new'
   resources :games do
     resources :stats, only: [:index, :create, :destroy]
   end
