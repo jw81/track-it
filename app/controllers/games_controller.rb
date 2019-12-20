abc53a58e35bb64bb57c2799126dc754bebf8b67
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2019-12-20 10:41:40 -0600

    Gets the most basic stat tracking logic working
    - Going to refactor, but want this commit saved off incase I break something along the way.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    respond_to do |format|
+      if @game.save
+        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { render :new }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:opponent_id, :location_id, :notes)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :games do
+    resources :stats, only: [:index, :create]
+  end
+  resources :stats, only: [:destroy]
 end
