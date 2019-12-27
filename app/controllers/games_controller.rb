6296d0cc8498e7a0ddcac5bd67dfb0a555d70895
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2019-12-27 17:38:41 -0600

    provide ability to edit game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,13 @@
 class GamesController < ApplicationController
-  def show
-    @game = Game.find(params[:id])
-  end
+  before_action :set_game, only: %i[show edit update]
+
+  def show; end
 
   def new
     @game = Game.new
   end
+
+  def edit; end
 
   def create
     @game = Game.new(game_params)
@@ -21,7 +23,23 @@ class GamesController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @game.update(game_params)
+        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.json { render :show, status: :ok, location: @game }
+      else
+        format.html { render :edit }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
 
   def game_params
     params.require(:game).permit(:opponent_id, :location_id, :notes)
